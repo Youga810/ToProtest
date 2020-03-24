165075c338f0d6dc6ee383b8553d15c1e5a3107f
Youga810 <pron.youg810@gmail.com>
2020-03-25 06:11:13 +0900

    ホームのhtmlファイルを作成

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) =>{
-  res.writeHead(200, {'Content-Type' : 'text/plain'});
-  
-  res.write('hello world!');
-  res.end();
+  fs.readFile('./index.html', 'utf-8', (err, data) =>{
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write(data);
+    console.log(req.url);
+    res.end();
+  }
+);
 });
 
 const port = 8000;
